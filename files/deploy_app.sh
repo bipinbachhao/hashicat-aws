8ece347a3770abd4c2222d7d8388f08d9a28f91e
Bipin Bachhao <bipinbachhao@gmail.com>
2019-10-24 17:58:55 +0000

    Updated website commit -2

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. THIS is new test text with my own content. 
+  Welcome to ${PREFIX}'s app. THIS is new test text with my own content again doing it. 
   <!-- END -->
   
   </div>
